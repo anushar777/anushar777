438aa57c4c65605aba82f963e70fff0c59a13a7d
anushar777 <anusha.ratra@gmail.com>
2024-08-14 12:29:49 -0400

    Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -12,7 +12,7 @@ class Calculator:
 
         self.equation = StringVar()
         self.entry_value = ' '
-        Entry(width=22, bg = '#262625', font = ('Anek Devanagari', 28),fg ='white', textvariable = self.equation).place(x=0, y=0)
+        Entry(width=22, bg = '#262625', font = ('Anek Devanagari', 29),fg ='white', textvariable = self.equation).place(x=0, y=0)
 
         Button(width = 11, height = 4, text = '(', relief = 'groove', bg = '#9acbfc', command = lambda:self.show('(')).place(x=0, y=50)
         Button(width = 11, height = 4, text = ')', relief = 'groove', bg = '#9acbfc', command = lambda:self.show(')')).place(x=90, y=50)
@@ -27,47 +27,55 @@ class Calculator:
         Button(width = 11, height = 4, text = '8', relief = 'groove', bg = '#a3d0d6', command = lambda:self.show('8')).place(x=180, y=275)
         Button(width = 11, height = 4, text = '9', relief = 'groove', bg = '#a3d0d6', command = lambda:self.show('9')).place(x=90, y=275)
         Button(width = 11, height = 4, text = '0', relief = 'groove', bg = '#a3d0d6', command = lambda:self.show('0')).place(x=90, y=350)
-        Button(width = 11, height = 4, text = '.', relief = 'groove', bg = '#9acbfc', command = lambda:self.show('.')).place(x=180, y=350)
+        Button(width = 11, height = 4, text = '.', relief = 'groove', bg = '#9acbfc', command = lambda:self.show('.')).place(x=270, y=350)
         Button(width = 11, height = 4, text = '+', relief = 'groove', bg = '#9acbfc', command = lambda:self.show(' + ')).place(x=270, y=275)
         Button(width = 11, height = 4, text = '-', relief = 'groove', bg = '#9acbfc', command = lambda:self.show(' - ')).place(x=270, y=200)
         Button(width = 11, height = 4, text = '/', relief = 'groove', bg = '#9acbfc', command = lambda:self.show(' / ')).place(x=270, y=50)
         Button(width = 11, height = 4, text = 'x', relief = 'groove', bg = '#9acbfc', command = lambda:self.show(' * ')).place(x=270, y=125)
+        Button(width = 11, height = 4, text = 'π', relief = 'groove', bg = '#9acbfc', command = lambda:self.show(' π ')).place(x=360, y=350)
 
         Button(width = 11, height = 4, text = 'tan()', relief = 'groove', bg = '#9acbfc', command = self.tan).place(x=360, y=50)
         Button(width = 11, height = 4, text = 'sin()', relief = 'groove', bg = '#9acbfc', command = self.sin).place(x=360, y=125)
         Button(width = 11, height = 4, text = 'cos()', relief = 'groove', bg = '#9acbfc', command = self.cos ).place(x=360, y=200)
         Button(width = 11, height = 4, text = '^', relief = 'groove', bg = '#9acbfc', command = lambda:self.show(' ^ ')).place(x=360, y=275)
 
-        Button(width = 11, height = 4, text = '=', relief = 'groove', bg = '#f59393', command = self.solve).place(x=270, y=350)
+        Button(width = 11, height = 4, text = '=', relief = 'groove', bg = '#f59393', command = self.solve).place(x=180, y=350)
         Button(width = 11, height = 4, text = 'C', relief = 'groove',bg ='#fabd7f', command = self.clear).place(x=0, y=350)
 
     def tan(self):
         a =0
         if self.entry_value != ' ':
             a = int(self.entry_value)
-        result = math.tan(a)
+            self.entry_value = str(self.entry_value)
+        result = round(math.tan(a), 3)
+        self.entry_value = str(result)
         self.equation.set(result)
 
     def sin(self):
         a =0
         if self.entry_value != ' ':
             a = int(self.entry_value)
-        result = math.sin(a)
+            self.entry_value = str(self.entry_value)
+        result = round(math.sin(a), 3)
+        self.entry_value = str(result)
         self.equation.set(result)
 
     def cos(self):
         a =0
         if self.entry_value != ' ':
             a = int(self.entry_value)
-        result = math.cos(a)
+            self.entry_value = str(self.entry_value)
+        result = round(math.cos(a), 3) 
+        self.entry_value = str(result)
         self.equation.set(result)
     
     def power(self):
         a = self.entry_value.split()
-        result = int(a[0]) ** int( a[2])
+        result = float(a[0]) ** float(a[2])
         self.equation.set(result)
 
     def show(self, value):
+        self.entry_value = str(self.entry_value)
         self.entry_value += str(value)
         self.equation.set(self.entry_value)
     
@@ -81,9 +89,9 @@ class Calculator:
             self.power()
         else:
             result = eval(self.entry_value)
+            self.entry_value = result 
             self.equation.set(result)
     
-
 
 
 
